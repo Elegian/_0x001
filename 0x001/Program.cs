ed9b0ecc8b8d5a1f204f06be52f7bec0015e7e5a
sanderele1 <sanderele1@ebnett.no>
2016-08-23 21:11:33 +0200

    Added basic OpenGL window

--- 0x001/Program.cs
+++ 0x001/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
 namespace _0x001
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ToolkitOptions Options = new ToolkitOptions() { Backend = PlatformBackend.PreferNative, EnableHighResolution = true };
+            using (Toolkit.Init(Options))
+            using (GameWindow MainWindow = new GameWindow())
+            {
+                MainWindow.Run();
+            }
         }
     }
 }
