52ab6b5cff18705a857daf70d14110a7ddb36e88
Elegian <n.kritharas@aol.com>
2016-08-25 21:42:07 +0200

    - Played with Square - Implemented Load Texture - Build own view

--- 0x001/Program.cs
+++ 0x001/Program.cs
@@ -17,11 +17,22 @@ namespace _0x001
     {
         static void Main(string[] args)
         {
-            ToolkitOptions Options = new ToolkitOptions() { Backend = PlatformBackend.PreferNative, EnableHighResolution = true };
-            using (Toolkit.Init(Options))
-            using (OpenGLExampleBaseWindow MainWindow = new OpenGLExampleBaseWindow())
+            bool areYouElegian = false;
+
+            if(areYouElegian)
             {
-                MainWindow.Run();
+                _0x001.Dev.Elegian.TestDisplay td = new Dev.Elegian.TestDisplay(640, 640);
+                td.Run();
+
+            }
+            else
+            {
+                ToolkitOptions Options = new ToolkitOptions() { Backend = PlatformBackend.PreferNative, EnableHighResolution = true };
+                using (Toolkit.Init(Options))
+                using (OpenGLExampleBaseWindow MainWindow = new OpenGLExampleBaseWindow())
+                {
+                    MainWindow.Run();
+                }
             }
         }
     }
