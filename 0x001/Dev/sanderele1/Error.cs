1f38c33b0abb6a4e2f96d4374ba823d9490c7085
sanderele1 <sanderele1@ebnett.no>
2016-08-28 20:05:35 +0200

    Added texture class to handle loading images onto the gpu

---
+++ 0x001/Dev/sanderele1/Error.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK.Graphics.OpenGL;
+
+namespace _0x001.Dev.sanderele1
+{
+    public static class Error
+    {
+
+        /// <summary>
+        /// Will print an error to console with the specified error code.
+        /// NOTE: Will not print anything if the errorcode is <see cref="ErrorCode.NoError"/>! 
+        /// </summary>
+        /// <param name="code"></param>
+        public static void Print(ErrorCode code)
+        {
+            if (code == ErrorCode.NoError)
+                return;
+
+            Console.WriteLine("An error occured with the code: " + Enum.GetName(typeof(ErrorCode), code) + ", stacktrace: ");
+            Console.WriteLine(Environment.StackTrace);
+        }
+
+    }
+}
