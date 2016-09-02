684fb7ca61b9ded3078ed2496092ec179f5a7c08
Elegian <n.kritharas@aol.com>
2016-09-02 16:46:21 +0200

    - deleted useless code

--- 0x001/Program.cs
+++ 0x001/Program.cs
@@ -17,23 +17,12 @@ namespace _0x001
     {
         static void Main(string[] args)
         {
-            //bool areYouElegian = false;
-
-            //if(areYouElegian)
-            //{
-            //    _0x001.Dev.Elegian.TestDisplay td = new Dev.Elegian.TestDisplay(640, 640);
-            //    td.Run();
-
-            //}
-            //else
-            //{
                 ToolkitOptions Options = new ToolkitOptions() { Backend = PlatformBackend.PreferNative, EnableHighResolution = true };
                 using (Toolkit.Init(Options))
                 using (OpenGLExampleBaseWindow MainWindow = new OpenGLExampleBaseWindow())
                 {
                     MainWindow.Run();
                 }
-            //}
         }
     }
 }
